406f5966ae646f2dc2cf5ce91498aca09fb6c488
Michael Nunan <michael.nunan@outlook.com>
2023-12-13 21:18:18 -0600

    post, tag, and comment forms

---
+++ components/forms/CommentForm.js
@@ -0,0 +1,85 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+import { createComment, updateComment } from '../../utils/data/commentRequests';
+
+const iniitialState = {
+  content: '',
+};
+
+export default function CommentForm({
+  id,
+  user,
+  postId,
+  content,
+  createdOn,
+}) {
+  const [comment, setComment] = useState(iniitialState);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (id) {
+      setComment({
+        authorId: user.uid,
+        postId,
+        content,
+        createdOn,
+      });
+    }
+  }, [user.uid, content, createdOn, id, postId]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setComment((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (id) {
+      updateComment(id, comment).then(() => router.push('/'));
+    } else {
+      createComment({
+        ...comment,
+        createdOn: Date.now(),
+        authorId: user.uid,
+        postId,
+      }).then(() => router.push('/'));
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>Please, chime in</Form.Label>
+        <Form.Control
+          name="content"
+          type="text"
+          rows={2}
+          value={comment.content}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+    </Form>
+  );
+}
+
+CommentForm.propTypes = {
+  user: PropTypes.shape({
+    uid: PropTypes.string.isRequired,
+  }).isRequired,
+  id: PropTypes.string,
+  postId: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  createdOn: PropTypes.string,
+};
+
+CommentForm.defaultProps = {
+  id: null,
+  content: '',
+  createdOn: '',
+};
---
+++ components/forms/PostForm.js
@@ -0,0 +1,114 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+import { createPost, updatePost } from '../../utils/data/postRequests';
+
+const initialState = {
+  title: '',
+  imageUrl: '',
+  content: '',
+  approved: true,
+};
+
+export default function PostForm({
+  user,
+  id,
+  title,
+  publicationDate,
+  imageUrl,
+  content,
+  approved,
+}) {
+  const [currentPost, setCurrentPost] = useState(initialState);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (id) {
+      setCurrentPost({
+        rareUserId: user.uid,
+        title,
+        publicationDate,
+        imageUrl,
+        content,
+        approved,
+      });
+    }
+  }, [approved, content, id, imageUrl, publicationDate, title, user.uid]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentPost((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (id) {
+      updatePost(id, currentPost).then(() => router.push('/'));
+    } else {
+      createPost({ ...currentPost, publicationDate: Date.now(), rareUserId: user.uid }).then(() => router.push('/'));
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>What are you calling your post?</Form.Label>
+        <Form.Control
+          name="title"
+          type="text"
+          value={currentPost.title}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Add an image</Form.Label>
+        <Form.Control
+          name="imageUrl"
+          type="url"
+          placeholder="Image URL"
+          value={currentPost.imageUrl}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Speak your mind</Form.Label>
+        <Form.Control
+          name="content"
+          type="text"
+          rows={4}
+          value={currentPost.content}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+    </Form>
+
+  );
+}
+
+PostForm.propTypes = {
+  user: PropTypes.shape({
+    uid: PropTypes.string.isRequired,
+  }).isRequired,
+  id: PropTypes.number,
+  title: PropTypes.string,
+  publicationDate: PropTypes.string,
+  imageUrl: PropTypes.string,
+  content: PropTypes.string,
+  approved: PropTypes.bool,
+};
+
+PostForm.defaultProps = {
+  id: null,
+  title: '',
+  publicationDate: '',
+  imageUrl: '',
+  content: '',
+  approved: true,
+};
