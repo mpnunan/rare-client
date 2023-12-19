00dcaa19b3bd1625d1971d0174fe69cf55f0c447
melissapennington <mlpennington9@gmail.com>
2023-12-18 21:09:14 -0600

    getting things updated

---
+++ pages/details/index.js
@@ -0,0 +1,9 @@
+import DetailsPageComponent from '../../components/DetailsComponents';
+
+export default function DetailsPage() {
+  return (
+    <div>
+      <DetailsPageComponent />
+    </div>
+  );
+}
