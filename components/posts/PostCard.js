1bda6a9e4e48d11116fdac724a2d0e4b79a73ae4
Stacey Vanyo <sbvanyo@gmail.com>
2023-12-19 13:14:49 -0600

    light css

--- components/posts/PostCard.js
+++ components/posts/PostCard.js
@@ -11,7 +11,7 @@ function PostCard({ postObj }) {
         width: '18rem', margin: '20px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', borderRadius: '10px',
       }}
     >
-      <Card.Img variant="top" src={postObj.image_url} alt={postObj.title} style={{ height: '300px', borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }} />
+      <Card.Img variant="top" src={postObj.image_url} alt={postObj.title} style={{ height: 'auto', borderTopLeftRadius: '10px', borderTopRightRadius: '10px' }} />
       <Card.Body className="post-content" style={{ height: '200px' }}>
         <h5>{postObj.title}</h5>
         <p className="text-muted">{postObj.publication_date}</p>
